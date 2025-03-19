27eeee0baa52331dd26a4768f7dfe0ebfe7acefe
Arsaide <arsaidekm@gmail.com>
2025-03-19 23:41:07 +0200

    Init Pretty Slog package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Arsaide/prettyslog
+
+go 1.23.4
---
+++ prettyslog.go
@@ -0,0 +1,43 @@
+package prettyslog
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+)
+
+const (
+	Reset  = "\033[0m"
+	Red    = "\\e[0;31m"
+	Green  = "\\e[0;32m"
+	Yellow = "\\e[0;33m\t"
+	Blue   = "\\e[0;34m\t"
+)
+
+type ColorHandler struct {
+	slog.Handler
+}
+
+func NewColorHandler() *ColorHandler {
+	return &ColorHandler{}
+}
+
+func (h *ColorHandler) Handle(ctx context.Context, r slog.Record) error {
+	var color string
+	switch r.Level {
+	case slog.LevelInfo:
+		color = Green
+	case slog.LevelWarn:
+		color = Yellow
+	case slog.LevelError:
+		color = Red
+	case slog.LevelDebug:
+		color = Blue
+	default:
+		color = Reset
+	}
+
+	fmt.Printf("%s%s: %s%s\n", color, r.Level.String(), r.Message, Reset)
+
+	return nil
+}
